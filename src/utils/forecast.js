1acf4e1984726eca84b4ec2ae2fd21e30e9d1581
Pranab Pradhan <pranab.pradhan97@gmail.com>
2020-02-03 22:34:08 +0530

    added high and low temp

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         else if (response.body.error)
             callback('Unable to find location', undefined)
         else {
-            callback(undefined, response.body.daily.data[0].summary + 'it is currently ' + response.body.currently.apparentTemperature + ' degrees out there. There is ' + response.body.currently.precipProbability +'% chance of rain.')
+            callback(undefined, response.body.daily.data[0].summary + 'it is currently ' + response.body.currently.apparentTemperature + ' degrees out there. There is ' + response.body.currently.precipProbability +'% chance of rain. Todays high is '+ response.body.daily.data[0].temperatureHigh+' and todays low is '+ response.body.daily.data[0].temperatureLow +'.' )
         }
 
     })
